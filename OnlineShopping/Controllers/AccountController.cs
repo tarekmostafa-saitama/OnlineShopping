64c8a73c93e2a7ee2bf87f323ea8b46e49107ff2
Khairala <khairala@fci.helwan.edu.eg>
2020-06-28 17:59:54 +0200

    Final Merge
    Final Merge for phase 1

---
+++ OnlineShopping/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShopping.Core;
+using OnlineShopping.Persistence.ViewModels;
+
+namespace OnlineShopping.Controllers
+{
+
+    public class AccountController : Controller
+    {
+        IUnitOfWork unitOfWork;
+        HomeViewModel homeViewModel;
+
+        public AccountController(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+            homeViewModel = new HomeViewModel()
+            {
+                brands = null,
+                products = null,
+                categories = unitOfWork.CategoryRepository.GetAll(new string[] { }).ToList()
+            };
+        }
+
+        public IActionResult Login()
+        {
+            ViewBag.e = homeViewModel;
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            ViewBag.e = homeViewModel;
+            return View();
+        }
+    }
+}
