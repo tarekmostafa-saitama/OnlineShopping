cc8f06f99edcf3d3c7a7e98ae3d41e882c202afe
Tarek Mostafa <tarek.r.mostafa@gmail.com>
2020-06-28 04:07:06 +0200

    Finishing   -add new Product-   functionality

---
+++ OnlineShopping/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShopping.Areas.Admin.Models;
+using OnlineShopping.Core;
+using OnlineShopping.Core.DbEntities;
+
+namespace OnlineShopping.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public ProductsController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
+        }
+        [Route("Admin/Products/Add")]
+        public IActionResult Add()
+        {
+            ViewBag.Categories = _unitOfWork.CategoryRepository.GetAll(new string[0]).ToList();
+            ViewBag.Brands = _unitOfWork.BrandRepository.GetAll(new string[0]).ToList();
+            return View(new AddProductViewModel());
+        }
+        [Route("Admin/Products/Add")]
+        [HttpPost]
+        public IActionResult Add(AddProductViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Categories = _unitOfWork.CategoryRepository.GetAll(new string[0]).ToList();
+                ViewBag.Brands = _unitOfWork.BrandRepository.GetAll(new string[0]).ToList();
+                return View(model);
+            }
+
+            var product = new Product()
+            {
+                Title = model.Title,
+                Description = model.Description,
+                Price = model.Price ?? default,
+                BrandId = model.BrandId ?? default,
+                CategoryId = model.CategoryId ?? default
+            };
+            _unitOfWork.ProductRepository.Add(product);
+            _unitOfWork.Complete();
+            foreach (var image in model.Images)
+            {
+                var newName = Guid.NewGuid();
+                var path = _hostEnvironment.WebRootPath + "/Uploads/" + newName + image.FileName;
+                FileStream stream = new FileStream(path, FileMode.Create);
+                image.CopyTo(stream);
+                stream.Close();
+
+                var productImage = new ProductImage()
+                {
+                    Path = newName + image.FileName,
+                    ProductId = product.Id
+                };
+                _unitOfWork.ProductImageRepository.Add(productImage);
+                _unitOfWork.Complete();
+            }
+            return RedirectToAction("Add");
+        }
+    }
+}
---
+++ OnlineShopping/Areas/Admin/Models/AddProductViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace OnlineShopping.Areas.Admin.Models
+{
+    public class AddProductViewModel
+    {
+        [Required(ErrorMessage = "Title is required.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Description is required.")]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Price is required.")]
+        public int? Price { get; set; }
+        [Required(ErrorMessage = "Brand is required.")]
+        public int? BrandId { get; set; }
+        [Required(ErrorMessage = "Category is required.")]
+        public int? CategoryId { get; set; }
+        [Required(ErrorMessage = "Images are required.")]
+        public IFormFileCollection Images { get; set; }
+    }
+}
