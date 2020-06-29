b2de7e4ba8d9e2ad61ce7d8426462328c016805e
Khairala <khairala@fci.helwan.edu.eg>
2020-06-29 15:23:44 +0200

    ProductControllers

--- OnlineShopping/Controllers/HomeController.cs
+++ OnlineShopping/Controllers/HomeController.cs
@@ -59,22 +59,7 @@ namespace OnlineShopping.Controllers
             return View("GetCategoryItems", homeViewModel);
 
         }
-        [HttpGet]
-        [Route("/Home/Details/{id}")]
-        public IActionResult Details(int id)
-        {
-            var product = unitOfWork.ProductRepository.Get(id, new string[] { });
-
-            homeViewModel = new HomeViewModel()
-            {
-                brands = unitOfWork.BrandRepository.GetAll(new string[0] { }).ToList(),
-                categories = unitOfWork.CategoryRepository.GetAll(new string[0] { }).ToList(),
-                products = unitOfWork.ProductRepository.GetAll(new string[0] { }).ToList().Where(item => item.CategoryId == product.CategoryId && item.Id != id).Take(3).ToList()
-            };
-
-            homeViewModel.products.Add(product);
-            return View(homeViewModel);
-        }
+        
 
 
     }
