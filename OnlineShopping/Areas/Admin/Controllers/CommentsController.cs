5e1cfa38d9f48d377940bb5f0ad1201fe4c82729
Tarek Mostafa <tarek.r.mostafa@gmail.com>
2020-07-02 03:25:57 +0200

    View Product Comments Functionality

---
+++ OnlineShopping/Areas/Admin/Controllers/CommentsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShopping.Core;
+
+namespace OnlineShopping.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CommentsController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CommentsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [Route("Admin/Comments/List/{productId}")]
+        public IActionResult List(int productId)
+        {
+            var comments = _unitOfWork.CommentRepository.Find(x => x.ProductId == productId, new[] {"Member"}).ToList();
+            return PartialView("_CommentsDetails",comments);
+        }
+    }
+}
--- OnlineShopping/Core/DbEntities/Product.cs
+++ OnlineShopping/Core/DbEntities/Product.cs
@@ -24,6 +24,7 @@ namespace OnlineShopping.Core.DbEntities
         public ICollection<ProductImage> ProductImages { get; set; }
         public ICollection<OrderProductDetail> OrderProductDetails { get; set; }
         public ICollection<MemberProductFavourite> MemberProductFavourites { get; set; }
+        public ICollection<Comment> Comments { get; set; }
        
 
     }
