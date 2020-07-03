71ce7d7a02384989aa907cb5d5efa52c01e2bd52
Tarek Mostafa <tarek.r.mostafa@gmail.com>
2020-07-03 10:27:14 +0200

    Delete Comment Functionality

--- OnlineShopping/Areas/Admin/Controllers/CommentsController.cs
+++ OnlineShopping/Areas/Admin/Controllers/CommentsController.cs
@@ -22,5 +22,15 @@ namespace OnlineShopping.Areas.Admin.Controllers
             var comments = _unitOfWork.CommentRepository.Find(x => x.ProductId == productId, new[] {"Member"}).ToList();
             return PartialView("_CommentsDetails",comments);
         }
+        [Route("Admin/Comments/Delete/{commentId}")]
+        public IActionResult Delete(int commentId)
+        {
+            var url = HttpContext.Request.Path;
+            var comment = _unitOfWork.CommentRepository.Get(commentId, new string[0]);
+            _unitOfWork.CommentRepository.Delete(comment);
+            _unitOfWork.Complete();
+            return Redirect(url.ToString());
+        }
+
     }
 }
