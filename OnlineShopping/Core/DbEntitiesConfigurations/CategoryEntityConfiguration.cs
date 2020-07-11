51d9ad5683f04aa6ef2af2054c4e64575e8ba35b
Tarek Mostafa <tarek.r.mostafa@gmail.com>
2020-07-11 21:02:15 +0200

    First Try of Delete Fix Relation

---
+++ OnlineShopping/Core/DbEntitiesConfigurations/CategoryEntityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineShopping.Core.DbEntities;
+
+namespace OnlineShopping.Core.DbEntitiesConfigurations
+{
+    public class CategoryEntityConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasMany(c => c.Products).WithOne(c => c.Category).HasForeignKey(c => c.CategoryId)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
